348b8592bcac291790f5057142613814c7e49cdc
ponurakk <p0unrakg@int.pl>
2022-10-29 23:30:42 +0200

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="home">
-		
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde in, aliquid deserunt iure, molestias aperiam amet provident. Eaque, nostrum neque harum recusandae. Quos quo officiis soluta maxime incidunt, optio ipsa!</p>
 	</div>
 </template>
 <script>
