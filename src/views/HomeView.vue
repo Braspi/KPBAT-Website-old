5a1a366e7fb70d11f9c133db43559879de43c5e8
ponurakk <p0unrakg@int.pl>
2022-10-30 16:32:48 +0100

    smth

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,28 @@
             <p class="title">{{ $t("main.title") }}</p>
             <p class="desc">{{ $t("main.desc") }}</p>
         </main>
+        <div class="wrapper">
+            <section class="invert">
+                <div class="left">
+                    <p class="name"></p>
+                    <p class="title"></p>
+                    <p class="desc">
+
+                        KP BAT s'occupe de la mise en œuvre complète des projets, en essayant d'accompagner les clients à chaque étape des travaux. Au travail, le plus important pour nous est de répondre aux attentes des investisseurs, tout en veillant au fonctionnalisme, au confort et à l'esthétique de l'espace.
+
+                        Nous sommes spécialisés dans la rénovation d’appartements, maisons, bureaux et commerces.
+
+
+                        N’hésitez pas, écrivez ou appelez. Nous garantissons votre satisfaction et une approche individuelle de votre projet.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class="left">
+                    l
+                </div>
+            </section>
+        </div>
     </div>
 </template>
 <script>
