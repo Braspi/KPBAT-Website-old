6d20ca615bcfaa279fc6493db88aa74a35281ae9
ponurakk <p0unrakg@int.pl>
2022-10-30 13:58:57 +0100

    home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template lang="html">
     <div class="home">
-        <div class="back-ground">
-            <img src="..//assets/icon/zdj1.jpg">
-        </div>
+        <main>
+            
+        </main>
     </div>
 </template>
 <script>
