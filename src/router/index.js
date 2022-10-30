1f172ae1293ab9ba0ac45e318e27b792c200eac7
ponurakk <p0unrakg@int.pl>
2022-10-30 20:06:40 +0100

    Panel login

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
+import PanelLoginView from '@/views/panel/PanelLoginView.vue';
+import PanelView from '@/views/panel/PanelView.vue';
 import Error404 from '@/views/errors/404View.vue';
 
 const routes = [
     {
         path: '/',
         name: 'home',
-        component: HomeView
+        component: HomeView,
     },
     {
         path: '/:catchAll(.*)',
+        name: '404',
         component: Error404,
-        name: '404'
+    },
+    {
+        path: '/panel',
+        name: 'panelLogin',
+        component: PanelLoginView,
+        children: [
+            {
+                path: '',
+                name: 'panel',
+                component: PanelView,
+            },
+        ]
     }
 ]
 
---
+++ src/views/panel/PanelLoginView.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="panelLogin">
+		<main>
+			<p class="title">Login to Continue </p>
+			<div>
+				<img src="@/assets/icon/logo/LOGO2.jpg" alt="">
+				<form>
+					<span class="break">
+						<label for="user">Username</label>
+						<input type="text" id="user">
+					</span>
+					<span class="break">
+						<label for="pass">Password</label>
+						<input type="password" id="pass">
+					</span>
+					<input type="submit" value="Login">
+				</form>
+			</div>
+			<p class="copy">&copy; 2022 KP BAT. All rights Reserved</p>
+		</main>
+	</div>
+</template>
+
+<style scoped src="@/assets/style/views/panel/login.less" lang="less">
+</style>
