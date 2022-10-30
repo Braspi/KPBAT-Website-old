f7fc1a678b9d8ed4ac4d0d46e84ad0c8e6ba849d
Braspi <93686365+Braspi@users.noreply.github.com>
2022-10-30 16:15:05 +0100

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <Nav/>
   <router-view/>
+  <Footer/>
 </template>
 
 <script>
 import Nav from '@/components/Nav.vue';
+import Footer from "@/components/Footer";
 export default {
   name: 'App',
   components: {
-    Nav
+    Nav,Footer
   }
 }
 </script>
