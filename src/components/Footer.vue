5b076e6c6b64b4c91feba024c619df0f7e81ba61
ponurakk <p0unrakg@int.pl>
2022-11-08 20:13:04 +0100

    Better responsivity than before. Still needs fixes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,38 +1,40 @@
 <template lang="html">
-    <div class="footer">
-            <a href="">
-              <img src="@/assets/icon/logo/LOGO3.jpg" class="logo" alt="">
-            </a>
+	<div class="footer">
+		<router-link to="/">
+			<img src="@/assets/icon/logo/LOGO3.jpg" class="logo" alt="">
+		</router-link>
 
-            <div class="links">
-              <router-link to="#">{{ $t("footer.home") }}</router-link>
-              <router-link to="#">{{ $t("footer.gallery") }}</router-link>
-              <router-link to="#">{{ $t("footer.how-works") }}</router-link>
-              <router-link to="#">{{ $t("footer.contact") }}</router-link>
-            </div>
+		<div class="links">
+			<router-link to="/" class="hua">{{ $t("footer.home") }}</router-link>
+			<router-link to="/gallery" class="hua">{{ $t("footer.gallery") }}</router-link>
+			<router-link to="#" class="hua">{{ $t("footer.how-works") }}</router-link>
+			<a href="/#contact" class="hua">{{ $t("footer.contact") }}</a>
+		</div>
 
-            <div class="media">
-                <a href="https://www.facebook.com/kpbat"><font-awesome-icon icon="fa-brands fa-facebook" class="fai"/></a>
-                <a href="https://www.instagram.com/kp_bat/"><font-awesome-icon icon="fa-brands fa-instagram" class="fai"/></a>
-                <a href="https://www.pinterest.fr/kp_bat/"><font-awesome-icon icon="fa-brands fa-pinterest" class="fai"/></a>
-            </div>
+		<div class="media">
+			<a href="https://www.facebook.com/kpbat"><font-awesome-icon icon="fa-brands fa-facebook" class="fai"/></a>
+			<a href="https://www.instagram.com/kp_bat/"><font-awesome-icon icon="fa-brands fa-instagram" class="fai"/></a>
+			<a href="https://www.pinterest.fr/kp_bat/"><font-awesome-icon icon="fa-brands fa-pinterest" class="fai"/></a>
+		</div>
 
-            <hr size="1px">
+		<hr size="1px">
 
-            <div class="contact">
-              <p class="number">+33 6 38 61 77 95</p>
-              <p class="mail">support@kpbat.fr</p>
-            </div>
+		<div class="contact">
+			<p class="number">+33 6 38 61 77 95</p>
+			<p class="mail">support@kpbat.fr</p>
+		</div>
 
-
-            <div class="copyright">
-              <p>© 2022 KP BAT. All Rights Reserved</p>
-            </div>
-    </div>
+		<div class="copyright">
+			<p>&copy; 2022 KP BAT. All Rights Reserved</p>
+		</div>
+	</div>
 </template>
+
 <script>
 export default {
-  name: 'Footer'
+	name: 'Footer'
 }
 </script>
-<style scoped src="@/assets/style/components/footer.less" lang="less"></style>
+
+<style scoped src="@/assets/style/components/footer.less" lang="less">
+</style>
