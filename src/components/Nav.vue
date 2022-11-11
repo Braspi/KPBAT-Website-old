4b43a442879c6629a550e8340911c1784be3a4fe
ponurakk <p0unrakg@int.pl>
2022-11-11 15:54:08 +0100

    Revert "Finished nav on mobile/tablet"
    This reverts commit bcd6560905ac1a234cd6ac9892c86ae06353de6d.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -10,20 +10,17 @@
             <img src="@/assets/icon/flags/france.jpeg" @click="changeLang('fr')" alt="france">
             <img src="@/assets/icon/flags/united_kingdom.jpeg" @click="changeLang('en')" alt="united_kingdom" >
         </div>
-
-        <font-awesome-icon icon="fa-solid fa-bars" @click="showNav()" class="bars" size="3x"/>
-        
         <div class="burger">
-            <div class="inner" :class="[hidden ? 'hidden' : '']">
+            <div class="inner">
                 <div class="links">
                     <router-link to="/gallery" class="hua">{{ $t("nav.gallery") }}</router-link>
                     <router-link to="/" class="hua">{{ $t("nav.home") }}</router-link>
                     <a href="/#contact" class="hua">{{ $t("nav.contact") }}</a>
                 </div>
-                <h1>{{ $t("nav.language") }}:</h1>
+                <h1>Wybierz Język:</h1>
                 <div class="lang">
                     <img src="@/assets/icon/flags/france.jpeg" @click="changeLang('fr')" alt="france">
-                    <img src="@/assets/icon/flags/united_kingdom.jpeg" @click="changeLang('en')" alt="united_kingdom">
+                    <img src="@/assets/icon/flags/united_kingdom.jpeg" @click="changeLang('en')" alt="united_kingdom" >
                 </div>
             </div>
         </div>
@@ -32,17 +29,9 @@
 
 <script>
 export default {
-    data() {
-        return {
-            hidden: true,
-        }
-    },
     methods: {
         changeLang(lang) {
             this.$i18n.locale = lang;
-        },
-        showNav() {
-            this.hidden = !this.hidden;
         }
     }
 }
