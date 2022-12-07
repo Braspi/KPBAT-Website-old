49f780899c6756cb1078f072402db96c99259439
ponurakk <p0unrakg@int.pl>
2022-12-07 20:32:41 +0100

    fixes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,8 +2,8 @@
   <div class="nav">
     <img src="@/assets/icon/logo/LOGO3.jpg" alt="logo" class="logo">
     <div class="links">
-      <router-link to="/gallery" class="hua">{{ $t("nav.home") }}</router-link>
-      <router-link to="/" class="hua">{{ $t("nav.gallery") }} </router-link>
+      <router-link to="/" class="hua">{{ $t("nav.home") }} </router-link>
+      <router-link to="/gallery" class="hua">{{ $t("nav.gallery") }}</router-link>
       <a href="/#contact" class="hua">{{ $t("nav.contact") }}</a>
     </div>
     <div class="lang">
@@ -16,8 +16,8 @@
     <div class="burger">
       <div class="inner" :class="[hidden ? 'hidden' : '']">
         <div class="links">
-          <router-link to="/gallery" class="hua">{{ $t("nav.gallery") }}</router-link>
           <router-link to="/" class="hua">{{ $t("nav.home") }}</router-link>
+          <router-link to="/gallery" class="hua">{{ $t("nav.gallery") }}</router-link>
           <a href="/#contact" class="hua">{{ $t("nav.contact") }}</a>
         </div>
         <h1>{{ $t("nav.language") }}:</h1>
