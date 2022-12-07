147118b7305490c9e597911d189b95f57f5dfbd3
ponurakk <p0unrakg@int.pl>
2022-12-07 19:51:32 +0100

    Added kp-input component

--- src/views/panel/ManageUsersView.vue
+++ src/views/panel/ManageUsersView.vue
@@ -5,11 +5,19 @@
 			<div v-for="user in users" :key="user.id">
 				<div v-if="user.role == 'ROLE_ADMIN'" class="admin invert">{{ user.id }}. {{ user.email }}</div>
 			</div>
+			<div class="add">
+				<kp-input v-model="adminInput" label="Email"/>
+				<button @click="addUser('ROLE_ADMIN')">Add Admin</button>
+			</div>
 		</section>
 		<section>
 			<h1>Users:</h1>
 			<div v-for="user in users" :key="user.id">
 				<div v-if="user.role == 'ROLE_USER'" class="user">{{ user.id }}. {{ user.email }}</div>
+			</div>
+			<div class="add">
+				<kp-input v-model="userInput" label="Email"/>
+				<button @click="addUser('ROLE_USER')">Add User</button>
 			</div>
 		</section>
 	</div>
@@ -20,16 +28,38 @@ import axios from 'axios';
 export default {
 	data() {
 		return {
-			users: []
+			users: [],
+			userInput: "",
+			adminInput: "",
+			selectedAdmin: false,
+			selectedUser: false,
 		}
 	},
 	created() {
 		axios.get("/api/users").then((res) => {
-			console.log(res);
 			this.users = res.data;
 		}).catch((e) => {
 			console.error(e);
 		})
+	},
+	methods: {
+		addUser(type) {
+			let email = "";
+			if (type == 'ROLE_ADMIN') {
+				email = this.adminInput;
+			} else if (type == 'ROLE_USER') {
+				email = this.userInput;
+			}
+			axios.post("/api/users", { "name": 6, "email": email, "role": type }).then((res) => {
+				console.log(res);
+				alert(res.data.password)
+			}).catch((e) => {
+				if (e.response.data.errors.includes("email format is invalid!")) {
+					alert("idiota no")
+				}
+				console.error(e.response.data.errors);
+			})
+		}
 	}
 }
 </script>
