72320c3cfa78e415d4f2bd22b6a82579d192d15b
Braspi <93686365+Braspi@users.noreply.github.com>
2023-01-06 19:54:08 +0100

    kurwa-działa

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -110,7 +110,7 @@
                         </div>
                     </div>
                     <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1274.0990718208316!2d2.19013932765034!3d48.89889028349159!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6637ffc3534d3%3A0x9cf183962cbea637!2s85%20Bd%20du%20G%C3%A9n%C3%A9ral%20Leclerc%2C%2092000%20Nanterre%2C%20Francja!5e0!3m2!1spl!2spl!4v1672865373668!5m2!1spl!2spl" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83998.9185124463!2d2.347035!3d48.85885484999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2zUGFyecW8LCBGcmFuY2ph!5e0!3m2!1spl!2spl!4v1673031091660!5m2!1spl!2spl" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </section>
