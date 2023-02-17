2d53f822d82c1bb8bd36487daa9091205865b108
Braspi <93686365+Braspi@users.noreply.github.com>
2023-02-18 00:10:28 +0100

    services

--- src/views/OurServicesView.vue
+++ src/views/OurServicesView.vue
@@ -1,111 +1,43 @@
 <template>
-  <div class="">
+    <section>
     <div class="top">
       <p class="title">{{ $t("main.sections.s2.title") }}</p>
       <p class="desc">{{ $t("main.sections.s2.desc") }}</p>
     </div>
-    <section>
-        <div class="row">
-          <div class="box1">
-            <div class="service-wrap">
-              <div class="service-icon">
-                <i class="fas fa-layer-group"></i>
-              </div>
-              <h4>Web Application</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...</p>
-              <a href="#">Read More</a>
-            </div>
-          </div>
-          <div class="box2">
-            <div class="service-wrap">
-              <div class="service-icon">
-                <i class="far fa-chart-bar"></i>
-              </div>
-              <h4>Highly customizable</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...</p>
-              <a href="#">Read More</a>
-            </div>
-          </div>
-          <div class="box3">
-            <div class="service-wrap">
-              <div class="service-icon">
-                <i class="fas fa-database"></i>
-              </div>
-              <h4>Responsive design</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...</p>
-              <a href="#">Read More</a>
-            </div>
-          </div>
-          <div class="box4">
-            <div class="service-wrap">
-              <div class="service-icon">
-                <i class="fas fa-cogs"></i>
-              </div>
-              <h4>service & plugins</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...</p>
-              <a href="#">Read More</a>
-            </div>
-          </div>
-          <div class="box5">
-            <div class="service-wrap">
-              <div class="service-icon">
-                <i class="fas fa-chart-pie"></i>
-              </div>
-              <h4>Optimised for speed</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...</p>
-              <a href="#">Read More</a>
-            </div>
-          </div>
-          <div class="box6">
-            <div class="service-wrap">
-              <div class="service-icon">
-                <i class="fas fa-thumbs-up"></i>
-              </div>
-              <h4>Dedicated support</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...</p>
-              <a href="#">Read More</a>
-            </div>
-          </div>
-          <div class="box7">
-            <div class="service-wrap">
-              <div class="service-icon">
-                <i class="fas fa-thumbs-up"></i>
-              </div>
-              <h4>Dedicated support</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...</p>
-              <a href="#">Read More</a>
-            </div>
-          </div>
-          <div class="box8">
-            <div class="service-wrap">
-              <div class="service-icon">
-                <i class="fas fa-thumbs-up"></i>
-              </div>
-              <h4>Dedicated support</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...</p>
-              <a href="#">Read More</a>
-            </div>
-          </div>
-          <div class="box9">
-            <div class="service-wrap">
-              <div class="service-icon">
-                <i class="fas fa-thumbs-up"></i>
-              </div>
-              <h4>Dedicated support</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry...</p>
-              <a href="#">Read More</a>
-            </div>
+      <div class="wrapper">
+        <div class="content-box" v-for="(service, index) in services" :key="index">
+          <div class="card" @mouseover="mouseEnterHandle($event, require(`@/assets/img/ourservices/${index+1}.jpg`))" @mouseleave="mouseLeaveHandle">
+            <font-awesome-icon :icon="service.iconName"/>
+            <h2>{{ service.name }}</h2>
+            <p>{{ service.description }}</p>
           </div>
         </div>
+      </div>
     </section>
-
-
-  </div>
 </template>
 
 <script>
 export default  {
-  return: 0
+  data: () => ({
+    services: [
+      {
+        background: '@/assets/img/ourservices/1.jpg',
+        iconName: 'fa-solid fa-info',
+        name: 'Rénovation Extérieur',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text"
+      }
+    ]
+  }),
+  methods: {
+    mouseEnterHandle(event, image) {
+      if (event.target.tagName !== 'DIV') return;
+      event.target.style.background = `linear-gradient(45deg, rgba(42, 42, 42,0.7) 0%, rgba(69, 69, 69, 0.7) 100%), url('${image}')`
+      event.target.style.backgroundSize = `cover`
+    },
+    mouseLeaveHandle() {
+      event.target.style.background = `#fff`
+    }
+  }
 }
 </script>
 <style scoped src="@/assets/style/views/ourservices.less" lang="less"> </style>
